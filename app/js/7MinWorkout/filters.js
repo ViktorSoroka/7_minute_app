ebc9254fb3bb0227ca39376889f82515ff4cdba9
Viktor Soroka <viktor_soroka@epam.com>
2016-03-12 21:49:49 +0200

    add total workout duration and next meeting info

--- app/js/7MinWorkout/filters.js
+++ app/js/7MinWorkout/filters.js
@@ -1,3 +1,16 @@
-/**
- * Created by montana on 28.02.2016.
- */
+angular.module('7minWorkout').filter('secondsToTime', function () {
+    return function (input) {
+        var sec = parseInt(input, 10),
+            hours,
+            minutes,
+            seconds;
+
+        if (isNaN(sec)) return "00:00:00";
+
+        hours = Math.floor(sec / 3600);
+        minutes = Math.floor((sec - (hours * 3600)) / 60);
+        seconds = sec - (hours * 3600) - (minutes * 60);
+
+        return ("0" + hours).substr(-2) + ':' + ("0" + minutes).substr(-2) + ':' + ("0" + seconds).substr(-2);
+    }
+});
